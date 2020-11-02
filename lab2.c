e9482e4721004a53c2a47de92d00e87af5e8c05f
OwlinOhana <47669603+OwlinOhana@users.noreply.github.com>
2020-11-03 00:41:01 +0700

    Beautified С Code

--- lab2.c
+++ lab2.c
@@ -10,7 +10,7 @@ double lNum = 10.0;
 
 void Vector(float *matrix, float *vector, float *vectorIt)
 {
-    for (int i = 0; i < m; i++) 
+    for (int i = 0; i < m; i++)
     {
         vectorIt[i] = 0.0;
         for (int j = 0; j < n; j++)
@@ -21,7 +21,7 @@ void Vector(float *matrix, float *vector, float *vectorIt)
 
 /*void Vector(double *matrix, double *vector, double *vectorIt)
 {
-    for (int i = 0; i < m; i++) 
+    for (int i = 0; i < m; i++)
     {
         vectorIt[i] = 0.0;
         for (int j = 0; j < n; j++)
@@ -38,7 +38,7 @@ double wtime()
 
 /*void Vector(int *matrix, int *vector, int *vectorIt)
 {
-    for (int i = 0; i < m; i++) 
+    for (int i = 0; i < m; i++)
     {
         vectorIt[i] = 0.0;
         for (int j = 0; j < n; j++)
@@ -56,18 +56,18 @@ double test()
     vectorIt = malloc(sizeof(*vectorIt) * n);
 
     double t;
-    for (int i = 0; i < m; i++) 
+    for (int i = 0; i < m; i++)
     {
         for (int j = 0; j < n; j++)
             matrix[i * n + j] = rand() % 1000;
     }
     for (int j = 0; j < n; j++)
         vector[j] =rand() % 1000;
-    
-     t = wtime();
+
+    t = wtime();
     Vector(matrix, vector, vectorIt);
     t = (wtime() - t) ;
-	//printf("Elapsed time (serial): %.6f sec.\n", t);
+    //printf("Elapsed time (serial): %.6f sec.\n", t);
     free(matrix);
     free(vector);
     free(vectorIt);
@@ -76,49 +76,46 @@ double test()
 
 
 
-void Math() 
+void Math()
 {
-	double dispersion =0, average = 0,  deviation = 0, taskPerf = 0, absError = 0, relError = 0;
-	int insCount = n*(m+(n-1)) + m;
+    double dispersion =0, average = 0,  deviation = 0, taskPerf = 0, absError = 0, relError = 0;
+    int insCount = n*(m+(n-1)) + m;
     double testAr[10];
-	double t = test();
+    double t = test();
 
-	 for (int i = 0; i < 10; i++)
-	{
-		testAr[i] += test();
-	     average += testAr[i];
-     }
+    for (int i = 0; i < 10; i++)
+    {
+        testAr[i] += test();
+        average += testAr[i];
+    }
 
     for (int i = 0; i < 10; i++)
     {
         testAr[i] = test();
         average += testAr[i];
         taskPerf += (insCount / testAr[i]);
-		dispersion += pow(testAr[i] - average, 2);
+        dispersion += pow(testAr[i] - average, 2);
     }
-   
-    taskPerf = pow((1 / taskPerf), -1); 
-    
+
+    taskPerf = pow((1 / taskPerf), -1);
+
     deviation = sqrt (dispersion / lNum);
     average /= lNum;//среднеквадратичное отклонение
     absError = average - t;
     relError = (absError / average)*100;
 
-	FILE  *file;
+    FILE  *file;
 
-    file = fopen("itog.cvs", "a"); 
+    file = fopen("itog.cvs", "a");
     fprintf (file, "PModel: Intel(R) Core(TM) i3-6006U CPU @ 2.00GHz \n");
     fprintf( file, "Task: multiplying a matrix by a vector OpType: -O \nOpt float\nInsCount %d \n",  insCount);
     fprintf (file, "Timer: time()\nTime: %.6f \nLNum: 10 \nAvTime: %.6f \nAbsErr: %.6f \n", t, average, absError);
     fprintf (file, "RelErr: %.6f \nTaskPer: %.2f % \n\n", relError, taskPerf);
     fclose(file);
- }
+}
 
 int main ()
 {
-   Math();
-	return 0;
+    Math();
+    return 0;
 }
-
-
-
